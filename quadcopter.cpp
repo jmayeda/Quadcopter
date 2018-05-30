11acdae32df73626a8185bc9542b17929e38a914
jmayeda <jmayeda@github.com>
2018-05-29 23:39:19 -0700

    adding skeleton code

--- quadcopter.cpp
+++ quadcopter.cpp
@@ -1,42 +1,84 @@
-/**
+/*******************************************************************************
  * @file: quadcopter.cpp
  *
- * @brief: quadcopter flight controller for the ESW Quarterly Project 2018.
- * @date: 05/22/2018
- */
+ * @author:
+ * @about: Quadcopter flight controller for the ESW Quarterly Project 2018.
+ * @date:
+ ******************************************************************************/
 
 /**
- * TODO: Add any libraries you need here. Add comment to explain what the
+ * TODO: Add any libraries you need here. Add comments to explain what the
  *       library is used for.
  * Ex: #include <math.h>  // used for trigonometric functions sin(), cos()
  * Ex: #include <Wire.h>  // used for I2C communication with devices
  */
- #include <Arduino.h>
+#include <Wire.h> // I2C communication with devices
 
-/**
+/*******************************************************************************
  * TODO: Define constants here. There are two common ways to define constants
  *       in C.
  * Ex: #define GREEN_LED_PIN 10
  * Ex: #define PI 3.14
  * Ex: const int GREEN_LED_PIN = 10;
- */
- #define PI 3.14
- const float ANOTHER_PI = 3.14;
+ ******************************************************************************/
+#define PI 3.14
+const float ANOTHER_PI = 3.14;
+
+/*******************************************************************************
+ * TODO: Define Structs or other custom variable types here.
+ ******************************************************************************/
 
 /**
- * TODO: Add Global Variables here. Global variables can be accessed throughout
- *       the code.
+ * enum to designate whether or not the quadcopter motors are armed.
  */
-float globalVariable;
-
+typedef enum arm_state_t{
+  ARMED,
+  DISARMED
+}arm_state_t;
 
 /**
- * Setup
+ * Struct to hold all receiver signals. Should be 1000-2000 us pulses
+ */
+typedef struct rc_signals_t {
+  uint16_t CH1;
+  uint16_t CH2;
+  uint16_t CH3;
+  uint16_t CH4;
+}rc_signals_t;
+
+/**
+ * Struct to hold the roll, pitch, yaw deg/s data from the MPU6050.
+ */
+typedef struct gyro_t {
+  float roll;
+  float pitch;
+  float yaw;
+}gyro_t;
+
+/*******************************************************************************
+ * Global Variables
  *
- * @brief: runs once at startup and is used for initialize pins, serial monitor.
- *         keyword 'void' indicates that setup() does not return anything.
+ ******************************************************************************/
+gyro_t gyro;
+rc_signals_t rc_signal;
+arm_state_t arm_state;
+
+/*******************************************************************************
+ * Function Declaration
+ *
+ ******************************************************************************/
+void readGyro(gyro_t *gyro);
+int calculatePID();
+
+/**
+ *
+ * @brief: Setup runs once and is used to setup and initialize all of the pins, and
+ *         methods we will use later.
  */
-void setup() {
+void setup()
+{
+  /* TODO: Add anything needed for setup here. */
+
   /* Here are some examples of typical setup stuff */
 
   // pinMode(10, INPUT); // designate digital pin 10 as an input
@@ -46,12 +88,12 @@ void setup() {
 }
 
 /**
- * loop
  *
  * @brief: runs continuously and is the main part of the code. This is where
  *         everything happens.
  */
-void loop() {
+void loop()
+{
   /* loop Psuedocode
    *
    * 0. pollReceiver();
@@ -70,33 +112,32 @@ void loop() {
    *    write corrected pulse lengths to the ESC's to actuate the quadcopter.
    */
 
-    // initialize the gyro values variable
-    float gyroValues[3] = {0.0, 0.0, 0.0};
-
-    // update the gyro values by passing a pointer
-    updateGyro(&gyroValues);
-
-    // print the gyro values for debugging
-    for (int i = 0; i<3; i++) {
-      printf("%.1f \n", gyroValues[i]);
-    }
-
 }
 
-
-int calculatePID() {
-
+/**
+ * @brief: calculate the PID outputs for the roll, pitch, and yaw controllers.
+ * @return [description]
+ */
+int calculatePID()
+{
+  /* TODO: */
+  return 0;
 }
 
+/**
+ * @brief: Read the gyro angular rates in deg/s and pass to gyro data struct.
+ * @param gyro pointer to struct to hold gyro angular rates.
+ */
+void readGyro(gyro_t *gyro)
+{
+  // in C, "static" keyword initializes variable once and value persists between calls
+  static float gyroRaw[3];
 
-void updateGyro(float *gyroValues) {
-    const int NUM_GYRO_MEAS = 3;
+  /* TODO: read gyro here */
+  gyroRaw[3] = {99.0, 99.0, 99.0}; // NOTE: placeholder; remove
 
-    /* ...read gyro here... */
-    float gyroRaw[3] = {1.0, 2.0, 3.0}; // placeholder
-
-    // add the gyro values in the local scope to the gyroValues pointer
-    for (int i = 0; i < NUM_GYRO_MEAS; i++) {
-        *(gyroValues+i) = gyroRaw[i];
-    }
+  // add gyro data to the gyro signal struct
+  gyro->roll  = gyroRaw[0];
+  gyro->pitch = gyroRaw[1];
+  gyro->yaw   = gyroRaw[2];
 }
